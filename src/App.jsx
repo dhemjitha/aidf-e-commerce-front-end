bb46faae3a75095f80ff6a893331b04939178d2d
Dulran Hemjitha <dhemjitha@gmail.com>
2025-01-13 20:19:04 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl text-red-500 font-bold underline">Hello World</h1>
+      <Button>Click Me!</Button>
+    </>
+  )
+}
+
+export default App
